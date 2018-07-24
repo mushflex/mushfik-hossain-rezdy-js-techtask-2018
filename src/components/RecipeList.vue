99c129942aef5a609330612bcbea5738c3c42034
Mushfik Hossain <mushfik.hossain@gmail.com>
2018-07-25 01:53:40 +1000

    css and lint fixs for list page

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,17 +1,17 @@
 <template>
 	<div class="recipe-list">
+		<h1>Recipe List</h1>
 		<div class="loading" v-if="loading">
 			Loading...
 		</div>
 
 		<div v-if="!loading" class="content">
-			<h4>recipe list:</h4>
 			<ul>
-				<li v-for="freshRecipe in recipes.topRecipeList">
+				<li v-for="freshRecipe in recipes.topRecipeList" :key="freshRecipe.id">
 					<a href="#">{{ freshRecipe.title }}</a>
 				</li>
 
-				<li v-for="oldRecipe in recipes.bottomRecipeList">
+				<li v-for="oldRecipe in recipes.bottomRecipeList" :key="oldRecipe.id">
 					<a href="#">{{ oldRecipe.title }}</a>
 				</li>
 			</ul>
@@ -24,7 +24,6 @@ import _ from 'lodash'
 
 export default {
 	name: 'RecipeList',
-	props: [ 'buttonLabel' ],
 	data () {
 		return {
 			loading: true,
@@ -37,14 +36,14 @@ export default {
 		// fetch the data when the view is created and the data is
 		// already being observed
 		await fetch('static/ingredients.json')
-      .then(ingredients => ingredients.json())
-      .then(json => {
+			.then(ingredients => ingredients.json())
+			.then(json => {
 				this.ingredients = this.filterIngredients(json.ingredients);
 			});
 
 		await fetch('static/recipes.json')
-      .then(r => r.json())
-      .then(json => {
+			.then(r => r.json())
+			.then(json => {
 				this.recipes = this.filterRecipes(json.recipes);
 			});
 		this.loading = false;
@@ -58,7 +57,7 @@ export default {
 				// ingredient is before the useBy date
 				if ((today < useBy) && (today > bestBefore)) {
 					return ingredient;
-				} 
+				}
 			});
 
 			const freshIngredients = _.map(ingredients, (ingredient) => {
@@ -68,7 +67,7 @@ export default {
 					return ingredient;
 				}
 			});
-			
+
 			return {
 				oldIngredients: _.compact(oldIngredients),
 				freshIngredients: _.compact(freshIngredients)
@@ -82,28 +81,26 @@ export default {
 			const freshIngredientNames = _.mapValues(this.ingredients.freshIngredients, (ingredient) => {
 				return ingredient.title;
 			});
-			
+
 			let freshRecipes = [];
 			let oldRecipes = [];
-			
+
 			_.each(recipes, (recipe) => {
 				const recipeIngredients = recipe.ingredients;
 				// check each of the ingredients
 				let hideRecipe = false;
 				let freshRecipe = true;
-				
 				_.each(recipeIngredients, (recipeIngredientName) => {
 					const existsInOld = _.includes(oldIngredientNames, recipeIngredientName);
 					const existsInFresh = _.includes(freshIngredientNames, recipeIngredientName);
 					if (!hideRecipe) {
-						hideRecipe = (!existsInOld && !existsInFresh) ? true : false;
+						hideRecipe = (!existsInOld && !existsInFresh);
 						if (freshRecipe) {
-							freshRecipe = (existsInOld) ? false : true;
+							freshRecipe = !existsInOld;
 						}
 					}
 				});
-				
-				if (!hideRecipe){
+				if (!hideRecipe) {
 					freshRecipe ? freshRecipes.push(recipe) : oldRecipes.push(recipe);
 				};
 			});
@@ -123,49 +120,49 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 div {
-  width: 200px;
+	width: 200px;
 }
 
 .recipe-list {
 	margin: auto;
 }
- 
+
 h2 {
-  font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
-  margin: 0;
-  padding: 0;
+	font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
+	margin: 0;
+	padding: 0;
 }
- 
+
 ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
 }
- 
+
 li {
-  font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
-  border-bottom: 1px solid #ccc;
+	font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+	border-bottom: 1px solid #ccc;
 }
- 
+
 li:last-child {
-  border: none;
+	border: none;
 }
- 
+
 li a {
-  text-decoration: none;
-  color: #000;
-  display: block;
-  width: 200px;
- 
-  -webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
-  -moz-transition: font-size 0.3s ease, background-color 0.3s ease;
-  -o-transition: font-size 0.3s ease, background-color 0.3s ease;
-  -ms-transition: font-size 0.3s ease, background-color 0.3s ease;
-  transition: font-size 0.3s ease, background-color 0.3s ease;
+	text-decoration: none;
+	color: #000;
+	display: block;
+	width: 200px;
+
+	-webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+	-moz-transition: font-size 0.3s ease, background-color 0.3s ease;
+	-o-transition: font-size 0.3s ease, background-color 0.3s ease;
+	-ms-transition: font-size 0.3s ease, background-color 0.3s ease;
+	transition: font-size 0.3s ease, background-color 0.3s ease;
 }
- 
+
 li a:hover {
-  font-size: 30px;
-  background: #f6f6f6;
+	font-size: 30px;
+	background: #f6f6f6;
 }
 </style>
