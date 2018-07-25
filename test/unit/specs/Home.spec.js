d8cb2450d0df1f9fce89168794c233b38fd8f012
Mushfik Hossain <mushfik.hossain@gmail.com>
2018-07-25 10:30:58 +1000

    test for btn component

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import Home from '@/components/Home'
 
 describe('Home.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Home)
-    const btnLabel = 'home'
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.home').textContent)
-      .toEqual(btnLabel)
-  })
+	it('should render button', () => {
+		const Constructor = Vue.extend(Home)
+		const btnLabel = 'Whats for lunch'
+		const vm = new Constructor().$mount()
+		console.log(vm.$el.querySelector('.main-button').textContent)
+		expect(vm.$el.querySelector('.main-button').textContent)
+			.toEqual(btnLabel)
+	})
 })
